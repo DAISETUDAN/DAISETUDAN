ac0acde38f99c99b3083a7db07222a74b3acbe28
DA\nwuser <goroumakisimamu810@gmail.com>
2019-01-09 01:33:33 +0900

    メインシステムが完成しかかってるかも

---
+++ DAISETUDAN/Assets/sura/Scripts/LineCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0)) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        //ラインとの当たり判定
+        if (other.gameObject.tag == "Block") {
+
+            FindObjectOfType<drawPhysicsLine>().pena++;
+            Debug.Log("ダメ");
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Good") {
+            FindObjectOfType<drawPhysicsLine>().ok++;
+            Debug.Log("いいぞ。");
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ DAISETUDAN/Assets/sura/Scripts/drawPhysicsLine.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawPhysicsLine : MonoBehaviour {
+
+    public GameObject linePrefab;
+    GameObject line;
+    public float lineLength = 0.2f;
+    public float lineWidth = 0.1f;
+
+    int i;
+
+    /// <summary>
+    /// ダメなやつに触った判定
+    /// </summary>
+    public int pena;
+    /// <summary>
+    /// 良かった判定
+    /// </summary>
+    public int ok;
+    /// <summary>
+    /// クリア判定
+    /// </summary>
+    public int clear;
+
+    private Vector3 touchPos;
+
+    void Start() {
+        Init();
+    }
+
+    void Update() {
+        drawLine();
+    }
+
+    void Init() {
+        pena = 0;
+        ok = 0;
+        clear = 0;
+    }
+
+    void drawLine() {
+
+        if (Input.GetMouseButtonDown(0)) {
+            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            touchPos.z = 0;
+        }
+
+        if (Input.GetMouseButton(0)) {
+
+            Vector3 startPos = touchPos;
+            Vector3 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            endPos.z = 0;
+
+            if ((endPos - startPos).magnitude > lineLength) {
+                GameObject obj = Instantiate(linePrefab , transform.position , transform.rotation) as GameObject;
+                obj.transform.position = (startPos + endPos) / 2;
+                obj.transform.right = (endPos - startPos).normalized;
+
+                obj.transform.localScale = new Vector3((endPos - startPos).magnitude , lineWidth , lineWidth);
+
+                obj.transform.parent = this.transform;
+                i++;
+
+                touchPos = endPos;
+            }
+
+        }
+
+    }
+
+}
