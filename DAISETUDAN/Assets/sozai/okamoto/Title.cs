36d15a0be07574c2157b6ee9550b9ea243020527
DA\nwuser <daiki091548@gmail.com>
2019-01-23 01:13:52 +0900

    適当なシーン遷移

---
+++ DAISETUDAN/Assets/sozai/okamoto/Title.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("DescriptionScene");
+        }
+    }
+}
