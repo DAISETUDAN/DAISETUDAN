c3248c55c62f05dbc46bbedbb9baf888d32b7390
DA\nwuser <daiki091548@gmail.com>
2019-01-23 06:41:12 +0900

    kansei

---
+++ DAISETUDAN/Assets/koki/Script/Cokecounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cokecounter : MonoBehaviour {
+
+    int Result;
+    public GameObject score_object = null; // Textオブジェクト
+    // Use this for initialization
+    void Start()
+    {
+        Text score_text = score_object.GetComponent<Text>();
+
+        Result = Count.getCoke();
+        score_text.text = "" + Result;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- DAISETUDAN/Assets/koki/Script/Count.cs
+++ DAISETUDAN/Assets/koki/Script/Count.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Count : MonoBehaviour {
     public GameObject score_object = null; // Textオブジェクト
     public int scoreNum;
+    public static int scoreCount;
 	// Use this for initialization
 	void Start () {
         scoreNum = 0;
@@ -16,5 +17,10 @@ public class Count : MonoBehaviour {
         Text score_text = score_object.GetComponent<Text>();
         // テキストの表示を入れ替える
         score_text.text = "" + scoreNum;
+        scoreCount = scoreNum;
+    }
+    public static int getCoke()
+    {
+        return scoreCount;
     }
 }
