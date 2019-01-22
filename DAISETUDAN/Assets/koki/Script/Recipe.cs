c86f603f6affe62ba90e507ae41ca72719185ddd
kou5656 <kou.karu.ramu10@icloud.com>
2019-01-22 21:39:05 +0900

    コック

---
+++ DAISETUDAN/Assets/koki/Script/Recipe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Recipe : MonoBehaviour {
+    public Sprite[] recipeFood;
+    int recipeNum;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        recipeNum = FindObjectOfType<Cock>().rdm;
+
+        switch (recipeNum)
+        {
+            case 1:
+
+                this.gameObject.GetComponent<Image>().sprite = recipeFood[0];
+                break;
+            case 2:
+
+                this.gameObject.GetComponent<Image>().sprite = recipeFood[1];
+                break;
+            case 3:
+
+                this.gameObject.GetComponent<Image>().sprite = recipeFood[2];
+                break;
+        }
+    }
+}
